f6715a86bdafa648937e1fab856230b9182aef5e
youranstray@163.com <youranstray@163.com>
2015-12-11 10:00:10 +0800

    phpMyAdmin

--- processorder.php
+++ processorder.php
@@ -6,6 +6,18 @@
 <body>
 	<h1>Bob's Auto Parts</h1>
 	<h2>Order Results</h2>
-	<?php echo '<p>Order processed.</p>'; ?>
+	<?php
+	 // echo '<p>Order processed.</p>';
+
+	 $tireqty = $_POST['tireqty'];
+	 $oilqty = $_POST['oilqty'];
+	 $sparkqty = $_POST['sparkqty'];
+
+	 echo '<p>Your order is as follows: </p>';
+	 echo $_POST;
+	 echo $tireqty. ' tires<br>';
+	 echo $oilqty. ' bottles of oil<br>';
+	 echo $sparkqty. ' spark plugs<br>';
+	?>
 </body>
 </html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php     
+	$con=mysql_connect('127.0.0.1','root','admin'); //连接数据库       
+	if(!$con)       
+	{       
+	echo "fail";       
+	}       
+	else      
+	{       
+	echo "success";       
+	}       
+?>
